a89dea808d0240c37f7526521d727da6d3fd5eda
ramraj145 <ramwaghmode145@gmail.com>
2021-02-03 19:18:10 +0530

    added dob and gender

--- src/components/register.js
+++ src/components/register.js
@@ -15,8 +15,15 @@ const register = () => {
                 <input type="text"/><br/>
                 <label htmlFor="username">User Name</label>
                 <input type="text"/><br/>
-                <label htmlFor="qualification">Qualification</label>
-                <input type="text"/><br/>
+                <label htmlFor="dob">DOB</label>
+                <input type="date"/><br/>
+                <label htmlFor="gender">Gender</label>
+                <input type="radio" id="male" name="gender" value="male"/>
+                <label htmlFor="male">Male</label>
+                <input type="radio" id="female" name="gender" value="female"/>
+                <label htmlFor="female">Female</label>
+                <input type="radio" id="other" name="gender" value="other"/>
+                <label htmlFor="other">Other</label><br/>       
                 <label htmlFor="password">Password</label>
                 <input type="text"/><br/>
                 <label htmlFor="repassword">Re-Enter Password</label>
