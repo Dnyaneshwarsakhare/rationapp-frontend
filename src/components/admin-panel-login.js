ac3ad5f17e6d26721982abc1e53316201084f580
Dnyaneshwar Sakhare <46813756+Dnyaneshwarsakhare@users.noreply.github.com>
2022-05-26 12:20:05 +0530

    Updated admin credentials on admin login panel

--- src/components/admin-panel-login.js
+++ src/components/admin-panel-login.js
@@ -130,8 +130,8 @@ class AdminLogin extends PureComponent {
                         <input type="submit" value = "Login" className="btn btn-primary"/>
                     </div>
                 </form>
-                 <i> admin username : admin@gmail.com </i>
-                 <i> admin password : admin 12 <i>
+                <div> <i> admin username : admin@gmail.com </i>
+                 <i> admin password : admin 12 <i> </div>
             </div>
 
             <ToastContainer /> 
